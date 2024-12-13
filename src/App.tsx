0e834ce7db3fbf9e3da6fed961244c919e7c911d
Maxik-web <kleok0039@gmail.com>
2024-12-13 15:35:24 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
-import { FC } from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout';
+import { FC } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "./Layout";
 
-import { Product } from './pages/Product';
-import { CreateProduct } from './pages/CreateProduct';
-import { Products } from './pages/Products';
-
-export const routes = [
-  { path: '/products', element: <Products /> },
-  { path: '/products/:id', element: <Product /> },
-  { path: '/create-product', element: <CreateProduct /> },
-];
+import { Product } from "./pages/Product";
+import { CreateProduct } from "./pages/CreateProduct";
+import { Products } from "./pages/Products";
 
 const App: FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/products" />} />
       <Route
         path="*"
         element={
           <Layout>
             <Routes>
-              {routes.map((route, index) => (
-                <Route key={index} path={route.path} element={route.element} />
-              ))}
+              <Route path="/" element={<Navigate to="/products" replace />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/products/:id" element={<Product />} />
+              <Route path="/create-product" element={<CreateProduct />} />
             </Routes>
           </Layout>
         }
