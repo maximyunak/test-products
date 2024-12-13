a8d0a1d5b626d7b3e133edab09067d9ca5255482
Maxik-web <kleok0039@gmail.com>
2024-12-13 15:50:04 +0300

    fix input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,19 +1,19 @@
-import { useAppDispatch } from '@hooks';
-import { setSearchText } from '@store/productsSlice';
-import React, { ChangeEvent, useState } from 'react';
+import { useAppDispatch } from "@hooks";
+import { setSearchText } from "@store/productsSlice";
+import React, { ChangeEvent, useState } from "react";
 
 export const Search = () => {
-  const [setsearchText, setSetsearchText] = useState<string>('');
+  const [setsearchText, setSetsearchText] = useState<string>("");
   const dispatch = useAppDispatch();
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     dispatch(setSearchText(e.target.value));
   };
   return (
-    <div className="text- ">
+    <div className="">
       <input
         type="text"
         // className="bg-transparent border border-gray-500 py-1 px-3 rounded-lg"
-        className="transition border-gray-600 placeholder-gray-400 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-56 px-2.5 py-2 text-sm"
+        className="transition border-gray-600 placeholder-gray-400 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-56 px-2.5 py-2 text-sm max-[400px]:w-full"
         onChange={(e) => handleChange(e)}
         placeholder="Search by title and description"
       />
