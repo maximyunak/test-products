a8eed9b00dd229559bcad74d80fec0a7ccf798b0
Maxik-web <kleok0039@gmail.com>
2024-12-13 15:23:07 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,10 +10,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@hooks': resolve(__dirname, './src/store/hooks.ts'),
-      '@store': resolve(__dirname, './src/store'),
-      '@shared': resolve(__dirname, './src/shared'),
-      '@components': resolve(__dirname, './src/components'),
+      "@hooks": resolve(__dirname, "./src/store/hooks.ts"),
+      "@store": resolve(__dirname, "./src/store"),
+      "@shared": resolve(__dirname, "./src/shared"),
+      "@components": resolve(__dirname, "./src/components"),
     },
   },
 });
